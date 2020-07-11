2db7b018e1cdc55adcd6e1d76838536dabb3ae98
cescoferraro <francescoaferraro@gmail.com>
2020-07-11 18:33:40 -0300

    new fields expiriment

--- content/client/awesome-images.md
+++ content/client/awesome-images.md
@@ -1,6 +1,7 @@
 ---
 path: awesome-images
 date: 2020-06-06T01:22:55.506Z
+name: Awesome Images
 title: Awesome Images
 description: Fix for gatsby-plugin-mdx and gatsby-remark-images
 ---
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,66 @@
+import { graphql, navigate } from "gatsby"
 import * as React from "react"
-import {navigate} from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-class IndexPage extends React.Component<any,any> {
-    render() {
-        const siteTitle = "Gatsby Starter Personal Website"
+const IndexPage = (props: any) => {
+  const siteTitle = "Gatsby Starter Personal Website"
+  const clients = props.data.allMdx.edges
+  console.log(clients)
 
-        return (
-            <Layout location={this.props.location} title={siteTitle}>
-                <SEO
-                    title="Home"
-                    keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-                />
-                <button
-                    onClick={() => {
-                        console.log(33)
-                        navigate("/client")
-                    }}
-                >
-                    Go to Blog
-                </button>
-            </Layout>
-        )
-    }
+  return (
+    <Layout location={props.location} title={siteTitle}>
+      <SEO
+        title="Home"
+        keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+      />
+      {
+        clients.map((d, i: number)=>{
+          return (
+            <div key={i}>
+              <h2>{d.node.frontmatter.name}</h2>
+              <h2>{d.node.frontmatter.title}</h2>
+            </div>
+          )
+        })
+      }
+      <button
+        onClick={() => {
+          console.log(33)
+          navigate("/client")
+        }}
+      >
+        Go to Blog
+      </button>
+    </Layout>
+  )
 }
 
 export default IndexPage
+
+export const pageQuery = graphql`
+    query HomeQuery {
+        site {
+            siteMetadata {
+                title
+            }
+        }
+        allMdx(filter: {fileAbsolutePath: {regex: "/content/client/"}}) {
+            edges {
+                node {
+                    excerpt
+                    fields {
+                        slug
+                    }
+                    frontmatter {
+                        name
+                        date(formatString: "MMMM DD, YYYY")
+                        title
+                        description
+                    }
+                }
+            }
+        }
+
+    }
+`
