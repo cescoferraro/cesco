5076f51e3a529b7b14d2b3a1e3d1384b683c4d0c
cescoferraro <francescoaferraro@gmail.com>
2020-07-19 11:56:06 -0300

    refactor

--- src/components/drawerItem.tsx
+++ src/components/drawerItem.tsx
@@ -1,41 +1,31 @@
 import { createStyles, makeStyles } from "@material-ui/core/styles"
 import { navigate } from "gatsby"
 import React from "react"
-import * as classNames from "classnames"
+import * as cs from "classnames"
+
+const padding = "6px 10px"
 
 const itemStyles = makeStyles(() =>
   createStyles({
-    common: { fontSize: 13, lineHeight: "1em", padding: "6px 10px" },
-    selected: {
-      border: "1px solid #FFCC00",
-      color: "#AAA",
-      "&:hover": { color: "#000" },
-    },
-    regular: {
-      border: "1px solid transparent",
-      color: "#AAA",
-      "&:hover": { color: "#000" },
-    },
+    common: { color: "#AAA", fontSize: 13, lineHeight: "1em", padding },
+    selected: { border: "1px solid #FFCC00", "&:hover": { color: "#000" } },
+    regular: { border: "1px solid transparent", "&:hover": { color: "#000" } },
   }),
 )
-
-interface Props {
-  uri: string
-  label: string
-  page: string
-}
 
 export const TabComponent = ({ label, page, uri }: Props) => {
   const { common, regular, selected } = itemStyles()
   const here = uri === page
-  const className = classNames({
-    [common]: true,
-    [regular]: !here,
-    [selected]: here,
-  })
+  const className = cs({ [common]: true, [regular]: !here, [selected]: here })
   return (
     <li onClick={() => navigate(page)} className={className}>
       {label}
     </li>
   )
+}
+
+interface Props {
+  uri: string
+  label: string
+  page: string
 }
