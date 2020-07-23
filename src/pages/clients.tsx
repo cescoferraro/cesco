5ed1fcaafad7da1d84ca1d464a437e2f2a3347d1
cescoferraro <francescoaferraro@gmail.com>
2020-07-23 14:35:58 -0300

    trigguer CI again

--- src/pages/clients.tsx
+++ src/pages/clients.tsx
@@ -8,7 +8,6 @@ const Clients = (): React.ReactElement => {
       <div style={{ margin: "20px 0 40px" }}>
         <h2>todos clientes aqui</h2>
         <h2>todos clientes aqui</h2>
-        <h2>todos clientes aqui</h2>
       </div>
     </React.Fragment>
   )
