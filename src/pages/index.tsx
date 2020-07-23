56f11997a8ee92c0de4e4630618d5b8678d3dbd9
cescoferraro <francescoaferraro@gmail.com>
2020-07-23 13:09:03 -0300

    add categories tabs images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
-import { Box, Typography } from "@material-ui/core"
+import { Box } from "@material-ui/core"
 import AppBar from "@material-ui/core/AppBar"
+import GridList from "@material-ui/core/GridList"
+import GridListTile from "@material-ui/core/GridListTile"
+import GridListTileBar from "@material-ui/core/GridListTileBar"
+import IconButton from "@material-ui/core/IconButton"
+import { makeStyles } from "@material-ui/core/styles"
 import Tab from "@material-ui/core/Tab"
 import Tabs from "@material-ui/core/Tabs"
+import InfoIcon from "@material-ui/icons/Info"
 import { graphql } from "gatsby"
 import _ from "lodash"
 import * as React from "react"
@@ -13,10 +19,9 @@ interface Props {
   children: React.ReactElement
   value: index
   index: number
-  title: string
 }
 
-const TabPanel = ({ title, children, index, value }: Props) => {
+const TabPanel = ({ children, index, value }: Props) => {
   console.log(index, value)
   return (
     <div
@@ -25,20 +30,33 @@ const TabPanel = ({ title, children, index, value }: Props) => {
       id={`simple-tabpanel-${index}`}
       aria-labelledby={`simple-tab-${index}`}
     >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{title}</Typography>
-          {children}
-        </Box>
-      )}
+      {value === index && <Box p={3}>{children}</Box>}
     </div>
   )
 }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+  },
+  icon: {
+    color: "rgba(255, 255, 255, 0.54)",
+  },
+}))
+
 const IndexPage = (props: { data?: HomeQueryQuery }): React.ReactElement => {
   const projects = props.data.projects.edges
   const actualProject = projects.map((d) => d.node.frontmatter)
   const allProjects = _.groupBy(actualProject, "categorie")
   const categories = Object.keys(allProjects)
+  console.log(props)
   console.log(categories)
   const [value, setValue] = React.useState(0)
   const handleChange = (event, newValue) => setValue(newValue)
@@ -46,6 +64,8 @@ const IndexPage = (props: { data?: HomeQueryQuery }): React.ReactElement => {
     id: `simple-tab-${index}`,
     "aria-controls": `simple-tabpanel-${index}`,
   })
+  const classes = useStyles()
+
   return (
     <React.Fragment>
       <SEO title="Home" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
@@ -63,15 +83,29 @@ const IndexPage = (props: { data?: HomeQueryQuery }): React.ReactElement => {
       {categories.map((categorie, index) => {
         const categorieProjects = allProjects[categorie]
         return (
-          <TabPanel title={categorie} key={index} value={value} index={index}>
-            {categorieProjects.map((project, index) => {
-              return (
-                <div key={index}>
-                  <h2> {project.title} </h2>
-                  <h2> {project.image} </h2>
-                </div>
-              )
-            })}
+          <TabPanel key={index} value={value} index={index}>
+            <GridList cellHeight={180} className={classes.gridList}>
+              {categorieProjects.map((tile) => (
+                <GridListTile key={tile.image}>
+                  <img
+                    src={require("../../content/" + tile.image)}
+                    alt={tile.image}
+                  />
+                  <GridListTileBar
+                    title={tile.title}
+                    subtitle={<span>by: {tile.description}</span>}
+                    actionIcon={
+                      <IconButton
+                        aria-label={`info about ${tile.title}`}
+                        className={classes.icon}
+                      >
+                        <InfoIcon />
+                      </IconButton>
+                    }
+                  />
+                </GridListTile>
+              ))}
+            </GridList>
           </TabPanel>
         )
       })}
