9cd12ebeed55756f4eafb998e9840182976277ba
cescoferraro <francescoaferraro@gmail.com>
2020-07-23 14:28:52 -0300

    trigguer CI again

--- src/pages/clients.tsx
+++ src/pages/clients.tsx
@@ -7,6 +7,8 @@ const Clients = (): React.ReactElement => {
       <SEO title="All posts" />
       <div style={{ margin: "20px 0 40px" }}>
         <h2>todos clientes aqui</h2>
+        <h2>todos clientes aqui</h2>
+        <h2>todos clientes aqui</h2>
       </div>
     </React.Fragment>
   )
